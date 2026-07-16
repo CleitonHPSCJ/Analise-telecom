4002762ffb7269673dde7700d8b9e4a6f1206525
CleitonHPSCJ <cleitonvalerio48@gmail.com>
2026-07-15 21:54:20 -0300

    Update 04_receita_total_estado.sql

--- scripts/04_receita_total_estado.sql
+++ scripts/04_receita_total_estado.sql
@@ -1,1 +1,13 @@
-
+-- =============================================
+-- RECEITA TOTAL POR OPERADORA E ESTADO
+-- Objetivo: Identificar onde cada operadora
+-- fatura mais.
+-- =============================================
+SELECT tc.telecom_partner AS Operadoras,
+  tc.state AS Estados,
+  COUNT(*) AS Total_Cliente,
+  ROUND(SUM(p.valor_mensal) * COUNT(*), 2) AS Receita_total
+FROM telecom_churn tc 
+JOIN plans p on p.customer_id = tc.customer_id 
+GROUP BY tc.telecom_partner,tc.state 
+ORDER BY tc.telecom_partner, Receita_total DESC 
