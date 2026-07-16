3fe483e39c39bebc4d3ebf611ee4c1dda9a971d9
CleitonHPSCJ <cleitonvalerio48@gmail.com>
2026-07-15 21:10:11 -0300

    Update 02_analise_idade_genero.sql

--- scripts/02_analise_idade_genero.sql
+++ scripts/02_analise_idade_genero.sql
@@ -1,1 +1,13 @@
+-- =============================================
+-- ANÁLISE DE IDADE E GÊNERO POR OPERADORA
+-- Objetivo: Identificar o perfil demográfico
+-- de cada operadora.
+-- =============================================
 
+SELECT tc.telecom_partner AS Operadoras,
+  ROUND(AVG(tc.age )) AS Idade_media,
+  tc.gender AS Genero,
+  COUNT(*) AS Clienets
+FROM telecom_churn tc 
+GROUP BY tc.telecom_partner, tc.gender 
+ORDER BY tc.telecom_partner, Genero  DESC 
